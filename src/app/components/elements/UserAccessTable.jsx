6301d3235c8d73dbad1029ce734ff46e3422d657
Smittyvb <smittyvb@gmail.com>
2018-06-19 19:56:27 -0400

    Add UserAccessTable

---
+++ src/app/components/elements/UserAccessTable.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import shouldComponentUpdate from 'app/utils/shouldComponentUpdate';
+
+export default class UserAccessTable extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    shouldComponentUpdate = shouldComponentUpdate(this, 'UserAccessTable');
+    render() {
+        return (
+            <div>
+                <table className="userAccessTable">
+                    <thead>
+                        <tr>
+                            <td>Roles &amp; privileges</td><td>Owner</td><td>Admin</td><td>Mod</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Wallet access</td><td>✓</td><td></td><td></td>
+                        </tr>
+                        <tr>
+                            <td>Select moderators</td><td>✓</td><td>✓</td><td></td>
+                        </tr>
+                        <tr>
+                            <td>Remove posts</td><td>✓</td><td>✓</td><td>✓</td>
+                        </tr>
+                        <tr>
+                            <td>Select contributors</td><td>✓</td><td>✓</td><td>✓</td>
+                        </tr>
+                        <tr>
+                            <td>Block users</td><td>✓</td><td>✓</td><td>✓</td>
+                        </tr>
+                        <tr>
+                            <td>Change profile info</td><td>✓</td><td>✓</td><td>✓</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
